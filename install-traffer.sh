25a7ca659080be856ce37f6796bbc32ebe1680a5
Partner <gpartnerllc@gmail.com>
2026-07-27 20:01:57 -0300

    Add process

---
+++ install-traffer.sh
@@ -0,0 +1,8 @@
+# 1. Baixar o script do seu GitHub
+curl -O https://raw.githubusercontent.com/gpartnerllc-png/TRAFF/main/install-traffmonetizer.sh
+
+# 2. Dar permissão de execução
+chmod +x install-traffmonetizer.sh
+
+# 3. Executar (com 12 containers)
+TOKEN=UC15l5C72G5ulQPFyGX2DOum8utG9Y/PhPDQAnQDQmU= QUANTIDADE=12 DEVICE_NAME=Oracle PREFIXO=traff bash install-traffmonetizer.sh
